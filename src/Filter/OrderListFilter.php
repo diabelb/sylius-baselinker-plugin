f5570c64582c915b160634efd021803771e99305
Adam Terepora <adam@terepora.pl>
2023-07-11 17:55:36 +0200

    order mapper

--- src/Filter/OrderListFilter.php
+++ src/Filter/OrderListFilter.php
@@ -16,6 +16,27 @@ use Spinbits\SyliusBaselinkerPlugin\Rest\Input;
 
 class OrderListFilter extends AbstractFilter implements PaginatorFilterInterface
 {
+	public function getLimit(): int
+	{
+		$limit = (int) $this->get('filter_limit', 100);
+
+		if ($limit > 200) {
+			return 200;
+		}
+
+		if ($limit < 1) {
+			return 100;
+		}
+
+		return $limit;
+	}
+
+	public function getPage(): int
+	{
+		$page = (int) $this->get('page', 1);
+		return $page < 1 ? 1 : $page;
+	}
+
 	public function hasTimeFrom(): bool
 	{
 		return '' !== $this->getTimeFrom();
@@ -46,7 +67,7 @@ class OrderListFilter extends AbstractFilter implements PaginatorFilterInterface
 		$onlyPaid = $this->get('only_paid');
 		if ($onlyPaid && !in_array($onlyPaid, [0, 1])) return null;
 
-		return (bool) $onlyPaid;
+		return (int) $onlyPaid;
 	}
 
 	public function hasOrderId(): bool
