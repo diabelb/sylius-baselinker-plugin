87c16b50956c4212731b0b6759ec594a373381f8
Adam Terepora <adam@terepora.pl>
2023-07-12 11:42:18 +0200

    statuses fetched from machine state

--- src/Handler/StatusesListActionHandler.php
+++ src/Handler/StatusesListActionHandler.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @author Jakub Lech <info@smartbyte.pl>
+ * @author Adam Terepora <adam@terepora.pl>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -11,17 +11,34 @@ declare(strict_types=1);
 
 namespace Spinbits\SyliusBaselinkerPlugin\Handler;
 
-use Sylius\Component\Core\OrderCheckoutStates;
-use Spinbits\SyliusBaselinkerPlugin\Handler\HandlerInterface;
 use Spinbits\SyliusBaselinkerPlugin\Rest\Input;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class StatusesListActionHandler implements HandlerInterface
 {
+	const SYLIUS_ORDER_CONFIG_STATE_KEY = 'sylius_order';
+
+    protected TranslatorInterface $translator;
+
+    /** @var array */
+    protected $config;
+
+    public function __construct(TranslatorInterface $translator, array $config)
+    {
+        $this->translator = $translator;
+        $this->config = $config;
+    }
+
     public function handle(Input $input): array
     {
-        return [
-            OrderCheckoutStates::STATE_CART => 'Koszyk - w trakcie zamawiania',
-            OrderCheckoutStates::STATE_COMPLETED => 'Ukończone',
-        ];
+        if (!array_key_exists(self::SYLIUS_ORDER_CONFIG_STATE_KEY, $this->config)) {
+            throw new \RuntimeException("The provided state machine key is not configured.");
+        }
+        $states = $this->config[self::SYLIUS_ORDER_CONFIG_STATE_KEY]['states'];
+        $statesResponse = [];
+        foreach ($states as $state) {
+            $statesResponse[$state] = $this->translator->trans(sprintf('sylius.ui.%s', $state));
+        }
+        return $statesResponse;
     }
 }
